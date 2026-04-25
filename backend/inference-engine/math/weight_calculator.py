dffdd342924b33065c0a76829daad7003e02b9ae
Joshua Song <joshuasmsong@gmail.com>
2026-04-25 15:15:26 -0400

    weight stub

---
+++ backend/inference-engine/math/weight_calculator.py
@@ -0,0 +1,3 @@
+# calcs the weight of "allele" based on the user taste profle
+# weight is different per "allele" and is used in recommendation engine
+# user taste profile is found and created by WindowSampler.java and weight_caculator.py
