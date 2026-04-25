30d517791c8096ca24ec32b7273512d7b5ba6b47
Joshua Song <joshuasmsong@gmail.com>
2026-04-25 15:40:25 -0400

    user hisotry for voting and support

---
+++ backend/java-chassis/src/main/java/com/system/models/userSupportHistory.java
@@ -0,0 +1,6 @@
+/*
+    * obj for user_history.csv
+    * stores basic info about entry:
+    * titleId/pointer, timestamp, voting status, tags (List <String>) optional
+    * basically history of user support or voting status for politicians
+*/
