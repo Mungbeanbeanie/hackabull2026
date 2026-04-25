589edf868bf4e417c4962f5cc77604fbcb6c8a55
Joshua Song <joshuasmsong@gmail.com>
2026-04-25 15:37:47 -0400

    political figure bundler class stub

---
+++ backend/java-chassis/src/main/java/com/system/models/PoliFigure.java
@@ -0,0 +1,4 @@
+/*
+    * class that bundles a figure, their ID, and its Poli_Vector
+    * basically full obj of a given figure
+*/
---
+++ backend/java-chassis/src/main/java/com/system/models/PoliVector.java
@@ -0,0 +1,4 @@
+/*
+    * obj for allele vector
+    * basically stores the 10 values defiend in taxonomy.java
+*/
