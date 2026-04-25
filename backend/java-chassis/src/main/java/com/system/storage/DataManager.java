2eaa7dd683432df3f2db9dd7971f71d56685c8bd
Joshua Song <joshuasmsong@gmail.com>
2026-04-25 15:43:14 -0400

    datamangaer stub

---
+++ backend/java-chassis/src/main/java/com/system/storage/DataManager.java
@@ -0,0 +1,5 @@
+/*
+    * The "Gatekeeper." The only file that reads/writes the CSVs (eventually mongoDB) in the /data folder
+    * basically keeps the important data safe from corruption and messy logic
+    * later when move to a real database chng to SqlDatabaseManager.java logic
+*/
