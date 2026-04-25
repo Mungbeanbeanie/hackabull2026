6b37a1ec50c04a49e5aca67f6e7423cf143b8721
Joshua Song <joshuasmsong@gmail.com>
2026-04-25 15:21:13 -0400

    feat: add LibraryIndexer.java

---
+++ backend/java-chassis/src/main/java/com/system/managers/LibraryIndexer.java
@@ -0,0 +1,5 @@
+/*
+    * The core "Spatial Map." It builds the k-d tree in RAM during boot-up
+    * this will only be for local dev and prototyping later when moving to real database chng
+    * database will likely be vector db like pinecone or weaviate and this will be replaced by a database manager class that handles all interactions with the vector database
+*/
