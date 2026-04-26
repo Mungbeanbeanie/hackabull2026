146a4d80a459ae0e5410bd3c56d6b1a3e06aafd6
Joshua Song <joshuasmsong@gmail.com>
2026-04-26 03:26:30 -0400

    temp demoTarget for hack not for full

---
+++ backend/java-chassis/src/main/java/com/system/managers/IngestionRunner.java
@@ -0,0 +1,6 @@
+/*
+    *A one-time script that calls the APIs, 
+    * runs them through the LLM for scoring, and tells DataManager to SAVE them to the DB.
+    * for now should only be used to call until demoTarget.json is met for this prototype, 
+    * but can be extended to a more general "ingestion pipeline" in the future if desired.
+*/
